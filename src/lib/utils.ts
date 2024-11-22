ad5fde832b72d5eabdec211160f1d27b2b271de3
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-11-22 01:07:22 -0300

    fix generation of random colors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
 export function generateRandomColor() {
-	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	const hexColor = Math.floor(Math.random() * 16777215).toString(16);
+	// Pad with zeros if needed to ensure 6 digits
+	return '#' + hexColor.padStart(6, '0');
 }
