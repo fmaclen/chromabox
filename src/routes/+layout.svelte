b2c90a45b47ed560ca88ea32f04b9adee65ace02
Fernando Maclen <fmaclen@gmail.com>
2024-12-05 21:31:27 -0500

    feat: setup Cloudflare demo (#20)
    * feat: setup cloudflare demo
    
    * feat: add plausible script

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/public';
+</script>
+
+<svelte:head>
+	<title>Chromabox: An open-source color palette generator</title>
+
+	{#if env.PUBLIC_PLAUSIBLE_DOMAIN}
+		<script
+			defer
+			data-domain={env.PUBLIC_PLAUSIBLE_DOMAIN}
+			data-api={env.PUBLIC_PLAUSIBLE_API}
+			src={env.PUBLIC_PLAUSIBLE_SRC}
+		></script>
+	{/if}
+</svelte:head>
+
+<h1>Chromabox</h1>
+
+<hr />
+
+<main>
+	<slot />
+</main>
