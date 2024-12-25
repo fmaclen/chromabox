485fbdeaa0ea1ea2f7c29949a3438c3e0dafefdf
Fernando Maclen <fmaclen@gmail.com>
2024-12-25 11:26:08 -0500

    chore: add test for default palette in demo

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -10,4 +10,17 @@ test.describe('Demo', () => {
 		await page.goto('/');
 		await expect(page.getByText('Buy now')).toBeVisible();
 	});
+
+	test('default color palette is visible', { tag: '@demo' }, async ({ page }) => {
+		const color = page.locator('.color');
+
+		await page.goto('/');
+		await expect(color).toHaveCount(6);
+		await expect(color.nth(0).getByLabel('HEX', { exact: true })).toHaveValue('#008cff');
+		await expect(color.nth(1).getByLabel('HEX', { exact: true })).toHaveValue('#a600ff');
+		await expect(color.nth(2).getByLabel('HEX', { exact: true })).toHaveValue('#f600ff');
+		await expect(color.nth(3).getByLabel('HEX', { exact: true })).toHaveValue('#ff0004');
+		await expect(color.nth(4).getByLabel('HEX', { exact: true })).toHaveValue('#ff9000');
+		await expect(color.nth(5).getByLabel('HEX', { exact: true })).toHaveValue('#ffbf00');
+	});
 });
