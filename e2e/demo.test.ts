3d5de41da49e28ec4715308fbc24702d12b1dd42
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-12-30 11:26:39 -0300

    add tests for demo

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -23,4 +23,48 @@ test.describe('Demo', () => {
 		await expect(color.nth(4).getByLabel('HEX', { exact: true })).toHaveValue('#ff9000');
 		await expect(color.nth(5).getByLabel('HEX', { exact: true })).toHaveValue('#ffbf00');
 	});
+
+	test('JSON format is selected by default', { tag: '@demo' }, async ({ page }) => {
+		await page.goto('/');
+		const exportPanel = page.locator('aside.export-panel');
+
+		await page.getByRole('button', { name: 'New color' }).click();
+
+		await expect(exportPanel).toBeVisible();
+		await expect(exportPanel.getByRole('tab', { name: 'JSON', exact: true })).toHaveAttribute(
+			'aria-selected',
+			'true'
+		);
+		await expect(exportPanel.getByRole('tab', { name: 'CSS', exact: true })).not.toHaveAttribute(
+			'aria-selected',
+			'true'
+		);
+		await expect(exportPanel.getByRole('tab', { name: 'SCSS', exact: true })).not.toHaveAttribute(
+			'aria-selected',
+			'true'
+		);
+		await expect(
+			exportPanel.getByRole('tab', { name: 'Tailwind', exact: true })
+		).not.toHaveAttribute('aria-selected', 'true');
+	});
+
+	test('export formats other than JSON are locked', { tag: '@demo' }, async ({ page }) => {
+		await page.goto('/');
+		const exportPanel = page.locator('aside.export-panel');
+		const emptyMessage = ' export is only available in the full version.';
+
+		await page.getByRole('button', { name: 'New color' }).click();
+
+		await expect(exportPanel).toBeVisible();
+		await expect(page.getByText(emptyMessage)).not.toBeVisible();
+
+		await page.getByRole('tab', { name: 'CSS', exact: true }).click();
+		await expect(page.getByText('CSS' + emptyMessage, { exact: true })).toBeVisible();
+
+		await page.getByRole('tab', { name: 'SCSS', exact: true }).click();
+		await expect(page.getByText('SCSS' + emptyMessage, { exact: true })).toBeVisible();
+
+		await page.getByRole('tab', { name: 'Tailwind', exact: true }).click();
+		await expect(page.getByText('Tailwind' + emptyMessage, { exact: true })).toBeVisible();
+	});
 });
