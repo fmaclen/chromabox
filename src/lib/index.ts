c6ade6d6b34f53c7ed4e6b6669e1ef08cc5f4308
Fernando Maclen <fmaclen@gmail.com>
2025-01-07 13:29:46 -0500

    feat: add metatags and open graph (#81)
    * fix: update `STORE_URL`
    
    * feat: add metatags and open-graph
    
    * chore: apply formatting

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
 export const VIEWPORT_SIZE = { width: 1536, height: 1044 };
-export const STORE_URL = 'https://fmaclen.gumroad.com/l/chromabox';
+export const STORE_URL = 'https://store.fernando.is';
